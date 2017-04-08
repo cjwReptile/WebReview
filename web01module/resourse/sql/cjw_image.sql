bf25b401842fbc601d8e034dd367193bec2a5def
cjwReptile <836362264@qq.com>
2017-04-08 18:20:23 +0800

    cjw

---
+++ web01module/resourse/sql/cjw_image.sql
@@ -0,0 +1,25 @@
+﻿# Host: localhost  (Version 5.7.13-log)
+# Date: 2017-04-08 18:17:22
+# Generator: MySQL-Front 5.4  (Build 1.23)
+
+/*!40101 SET NAMES utf8 */;
+
+#
+# Structure for table "cjw_image"
+#
+
+DROP TABLE IF EXISTS `cjw_image`;
+CREATE TABLE `cjw_image` (
+  `image_id` int(11) NOT NULL AUTO_INCREMENT,
+  `image_url` varchar(50) NOT NULL DEFAULT '',
+  `image_name` varchar(50) DEFAULT NULL,
+  `user_id` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`image_id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `cjw_image_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `cjw_user` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+#
+# Data for table "cjw_image"
+#
+
---
+++ web01module/resourse/sql/cjw_user.sql
@@ -0,0 +1,23 @@
+﻿# Host: localhost  (Version 5.7.13-log)
+# Date: 2017-04-08 18:17:48
+# Generator: MySQL-Front 5.4  (Build 1.23)
+
+/*!40101 SET NAMES utf8 */;
+
+#
+# Structure for table "cjw_user"
+#
+
+DROP TABLE IF EXISTS `cjw_user`;
+CREATE TABLE `cjw_user` (
+  `user_id` int(10) NOT NULL AUTO_INCREMENT,
+  `pass_word` varchar(255) DEFAULT NULL,
+  `user_email` varchar(100) DEFAULT NULL,
+  `user_name` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户信息表';
+
+#
+# Data for table "cjw_user"
+#
+
